371d12a0e8e5d7a098f5c7a0da17d30603f4ab2a
Max Iyengar <max20038@gmail.com>
2022-02-06 16:08:19 +0000

    Implemented stations_within_radius (untested)

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,14 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+def stations_by_distance(stations, p):
+    ''' docstring '''
+    # Kyle todo
+    return [("test1", 5), ("test2", 15)] #for testing, remove after function implementation
+
+def stations_within_radius(stations, centre, r):
+    ''' return a list of stations within radius r from centre '''
+    stations_ordered = stations_by_distance(stations, centre)
+    return [i[0] if i[1] <= r else None for i in stations_ordered]
+    
