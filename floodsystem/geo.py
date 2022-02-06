bdf0ed16667f6dafc90ccaf21d24b7b096268252
Max Iyengar <max20038@gmail.com>
2022-02-06 23:42:56 +0000

    implemented rivers_by_station_number

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -31,16 +31,46 @@ def rivers_with_station(stations) -> set:
 
 
 def stations_by_river(stations) -> dict:
-    ''' returns stations organised by river in the form {river: "station"}'''
+    ''' returns stations organised by river in the form {river: ["station", "station"]}'''
     pass  # TODO Kyle
 
 
 def rivers_by_station_number(stations, N):
     ''' returns a list of N rivers with the greatest number of monitoring stations '''
 
-    greatest = 0  # greatest number of rivers
 
-    stations_by_river_dict = stations_by_river(stations)
+    #stations_by_river_dict = stations_by_river(stations)
 
+    stations_by_river_dict = {
+        "river a" : ["station", "station"],
+        "river b" : ["station", "station", "station", "station"],
+        "river c" : ["station", "station", "station"],
+        "river d" : ["station", "station", "station", "station"],
+        "river e" : ["station", "station", "station"],
+        "river f" : ["station", "station"],
+    }    # for testing while stations_by_river_dict not implemented
+
+    river_station_number = [] # A list of tuples in the form (river name, number of stations associated with that river)
+
+    # create river_station_number
     for i in stations_by_river_dict:
-        pass
+        river_station_number.append((i, len(stations_by_river_dict[i])))
+
+    greatest = 0 # greatest number of stations
+
+    # find greatest number of stations
+    for i in river_station_number:
+        if i[1] > greatest:
+            greatest = i[1]
+    
+    rivers = [] # rivers to be returned
+
+    while len(rivers) < N:
+
+        # add all rivers with 'greatest' number of rivers to the list
+        for i in river_station_number:
+            if i[1] == greatest:
+                rivers.append(i[0])
+        greatest -= 1
+
+    return rivers
