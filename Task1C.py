e27c50feb5096cda24bc8f9807fc4d6ae2ef9ffc
Max Iyengar <max20038@gmail.com>
2022-02-06 16:13:12 +0000

    Implemented Task 1C checker

---
+++ Task1C.py
@@ -0,0 +1,16 @@
+from floodsystem.geo import stations_within_radius
+from floodsystem.stationdata import build_station_list
+
+
+def run():
+    ''' Requirements for Task 1C'''
+    
+    stations = build_station_list()
+
+    # Print stations with 10km of Cambridge city centre:
+    print(stations_within_radius(stations, (52.2053, 0.1218), 10))
+
+
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
