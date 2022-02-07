1adc156a89f8d3308c8e5ba320af62980a59c7f6
kmc0707 <kmc0707@hotmail.com>
2022-02-07 03:13:39 +0000

    A few fixes for task 1b

--- Task1B.py
+++ Task1B.py
@@ -2,7 +2,7 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_distance
 
 def run():
-    """Requirements for Task 1A"""
+    """Requirements for Task 1B"""
 
     # Build list of stations
     stations = build_station_list()
