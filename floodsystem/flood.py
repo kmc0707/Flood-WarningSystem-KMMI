93f59d51e182da574b07a5591c81dad036382a63
Max Iyengar <max20038@gmail.com>
2022-03-03 16:23:14 +0000

    fixed 2b :)

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -6,12 +6,15 @@ This module contains functions related to flood detection
 def stations_level_over_threshold(stations, tol):
     "returns stations where water level is greater than tol as a list of tuples "
 
+    def l(x): return x[1]
+
     flooded_stations = []
 
     for i in stations:
         if i.relative_water_level() and i.relative_water_level() > tol:
             flooded_stations.append([i, i.relative_water_level()])
 
+    flooded_stations.sort(key=l, reverse = True)
     return flooded_stations
 
 
