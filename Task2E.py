39fa58dd18f02d5fad44bcf34637e78d5166215c
kmc0707 <kmc0707@hotmail.com>
2022-03-03 15:12:02 +0000

    2E fixed

--- Task2E.py
+++ Task2E.py
@@ -7,6 +7,8 @@ from floodsystem.datafetcher import fetch_measure_levels
 def sorts(station):
     if not station.latest_level:
         return 0.0
+    if station.latest_level > 100:
+        return 0.0
     return station.latest_level
 
 
@@ -14,12 +16,16 @@ def run():
     stations = build_station_list()
     update_water_levels(stations)
     stations.sort(key=sorts, reverse=True)
-    stations = stations[1:6]
+    count = 0
     for station in stations:
         dt = 10
         dates, levels = fetch_measure_levels(
         station.measure_id, dt=datetime.timedelta(days=dt))
-        plot_water_levels(station, dates, levels)
+        if dates:
+            plot_water_levels(station, dates, levels)
+            count = count + 1
+        if count == 5:
+            break
 
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
